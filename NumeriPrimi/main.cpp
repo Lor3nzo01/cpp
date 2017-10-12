0f149fb1d835a517f5a4efbaca64e4e249988df1
Lorenzo De Luca <32011163+Lor3nzo01@users.noreply.github.com>
2017-10-12 18:04:10 +0200

    Calcolatore di numeri primi

--- NumeriPrimi/main.cpp
+++ NumeriPrimi/main.cpp
@@ -1,24 +1,35 @@
 #include <iostream>
 #include <fstream>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
+    clock_t t_start, t_end;
+    double t_passed;
     cout<<"GENERATORE DI NUMERI PRIMI by Lorenzo De Luca"<<endl;
     unsigned long long max;
     cout<<"numero max(non deve essere obbligatoriamente primo): ";
     cin>>max;
     bool primi[max];
+    cout<<endl<<"Inizio del calcolo...";
+    t_start = clock();
     for (unsigned long long i=1;i<=max;i+=2) primi[i]=true;
     for(unsigned long long i=3; i*i<=max;i+=2)
     {
         for(unsigned long long l=i*2,m=3;l<=max;l=i*m,m++)primi[l]=false;
     }
+    t_end = clock();
+    t_passed = ((double)(t_end - t_start)) / CLOCKS_PER_SEC;
+    cout<<t_passed<<"seconds"<<endl<<"scrittura sul file...";
     ofstream file("primi.txt", ios::out);
-    file<<"2"<<endl; //il 2 è l'unico numero pari per cui per risparmiare tempo lo scriviamo direttamente
+    t_start = clock();
+    file<<"2"<<endl; //il 2 Ã¨ l'unico numero pari per cui per risparmiare tempo lo scriviamo direttamente
     for(unsigned long long i=3; i<=max;i+=2)if(primi[i]==true)file<<i<<endl;
+    t_end = clock();
+    t_passed = ((double)(t_end - t_start)) / CLOCKS_PER_SEC;
+    cout<<t_passed<<"seconds"<<endl<<"\\\\\\\\ END ////";
     file.close();
-    cout<<"THE END";
     return 0;
 }
