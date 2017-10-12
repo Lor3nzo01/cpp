3162304b3e390db8ad5ff419c2d9aeb68a5759b7
Lorenzo De Luca <32011163+lorenzodeluca@users.noreply.github.com>
2017-10-12 21:02:08 +0200

    Update main.cpp
    sintassi migliorata

--- NumeriPrimi/main.cpp
+++ NumeriPrimi/main.cpp
@@ -20,15 +20,13 @@ int main()
     {
         for(unsigned long long l=i*2;l<=max;l+=i)primi[l]=false;
     }
-    t_end = clock();
-    t_passed = ((double)(t_end - t_start)) / CLOCKS_PER_SEC;
+    t_end = clock();t_passed = ((double)(t_end - t_start)) / CLOCKS_PER_SEC;
     cout<<t_passed<<"seconds"<<endl<<"scrittura sul file...";
     ofstream file("primi.txt", ios::out);
     t_start = clock();
     file<<"2"<<endl; //il 2 è l'unico numero pari per cui per risparmiare tempo lo scriviamo direttamente
     for(unsigned long long i=3; i<=max;i+=2)if(primi[i]==true)file<<i<<"\n";
-    t_end = clock();
-    t_passed = ((double)(t_end - t_start)) / CLOCKS_PER_SEC;
+    t_end = clock();t_passed = ((double)(t_end - t_start)) / CLOCKS_PER_SEC;
     cout<<t_passed<<"seconds"<<endl<<"\\\\\\\\ END ////";
     file.close();
     return 0;
